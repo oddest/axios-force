6b19ec648b1a53dc50e5d9d26c31d4df8071b7f0
maker <maker.wu@outlook.com>
2019-07-29 15:32:32 +0800

    feat(index.ts): add entrance and define AxiosRequestConfig type

---
+++ src/types/index.ts
@@ -0,0 +1,22 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'head'
+  | 'HEAD'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
