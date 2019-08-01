aab44533bb170d7b17f90ef9cd87f10c9186e685
maker <maker.wu@outlook.com>
2019-08-01 16:24:58 +0800

    feat(examples): Add headers example

--- examples/base-data/app.ts
+++ examples/base-data/app.ts
@@ -9,10 +9,30 @@ axios({
   }
 })
 
-const arr =new Int32Array([21, 31])
+axios({
+  method: 'POST',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json',
+    'Accept': 'application/json, text/plain, */*'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 31])
 
 axios({
   method: 'POST',
   url: '/base/buffer',
   data: arr
+})
+
+const searchParams = new URLSearchParams('q=URLUtils.searchParams&topic=api')
+axios({
+  method: 'POST',
+  url: '/base/post',
+  data: searchParams
 })
