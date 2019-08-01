875794ba88c58228e8976b30d006693f038cf20a
maker <maker.wu@outlook.com>
2019-08-01 11:06:10 +0800

    feat(base example): Add base example for url transformer

--- examples/basic/app.ts
+++ examples/basic/app.ts
@@ -2,7 +2,7 @@ import axios from '../../src/index'
 
 axios({
   method: 'get',
-  url: '/simple/get',
+  url: '/basic/get',
   params: {
     a: 1,
     b: 2
--- examples/server.js
+++ examples/server.js
@@ -25,10 +25,14 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 const router = express.Router()
 
-router.get('/simple/get', function(req, res) {
+router.get('/basic/get', function(req, res) {
   res.json({
     msg: `hello world`
   })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
 })
 
 app.use(router)
